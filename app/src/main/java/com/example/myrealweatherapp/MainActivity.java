2db5f078ce77d3762d30a55af76e5a7ef608caf9
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-21 16:57:15 -0400

    Where I finished off friday.....lots done sofar. Still needs major styling.

--- app/src/main/java/com/example/myrealweatherapp/MainActivity.java
+++ app/src/main/java/com/example/myrealweatherapp/MainActivity.java
@@ -2,12 +2,13 @@ package com.example.myrealweatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -23,11 +24,16 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     TextView temperatureText;
-    ProgressBar progressBar;
     TextView temperature;
-
+    ImageView mainIcon;
+    TextView cityNameText;
+    Button queryButton;
+    TextView currentCity;
+    TextView weatherTypeName;
     static final String API_KEY = "37c0fb022a528bcc6afc1295558f1efb&units=metric";
     static final String API_URL = "https://api.openweathermap.org/data/2.5/forecast?q=kitchener,Canada&appid=";
+    String query = "";
+    String finalUrl = "";
 
 
     @Override
@@ -35,11 +41,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        temperatureText = (TextView) findViewById(R.id.locationText);
-        temperature = (TextView) findViewById(R.id.temperatureTextView);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        Button queryButton = (Button) findViewById(R.id.queryButton);
+        temperatureText = findViewById(R.id.locationText);
+        temperature = findViewById(R.id.temperatureTextView);
+        mainIcon = findViewById(R.id.imageView);
+        cityNameText = findViewById(R.id.locationString);
+        currentCity = findViewById(R.id.current);
+        mainIcon.setVisibility(View.GONE);
+        currentCity.setVisibility(View.GONE);
+//        weatherTypeName.findViewById(R.id.condition);
+        queryButton = findViewById(R.id.queryButton);
         queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -53,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         private Exception exception;
 
         protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
+
             temperature.setText("");
         }
 
@@ -73,24 +83,23 @@ public class MainActivity extends AppCompatActivity {
                     }
                     bufferedReader.close();
                     return stringBuilder.toString();
-                }
-                finally{
+                } finally {
                     urlConnection.disconnect();
                 }
-            }
-            catch(Exception e) {
+            } catch (Exception e) {
                 Log.e("ERROR", e.getMessage(), e);
                 return null;
             }
         }
 
+        @SuppressLint("SetTextI18n")
         protected void onPostExecute(String response) {
-            if(response == null) {
+            if (response == null) {
                 response = "THERE WAS AN ERROR";
             }
-            progressBar.setVisibility(View.GONE);
+
             Log.i("Response-->", response);
-            temperatureText.setText(response);
+//            temperatureText.setText(response);    USED TO SHOW THE RETURNED JSON DATA
             try {
 
                 JSONTokener jsonTokener = new JSONTokener(response);
@@ -98,22 +107,59 @@ public class MainActivity extends AppCompatActivity {
 
                 JSONObject mainObject = topObject.getJSONObject("main");
                 double temp = mainObject.getDouble("temp");
+                String cityName = topObject.getString("name");
 
-                JSONArray weatherArray =  topObject.getJSONArray("weather");
+                JSONArray weatherArray = topObject.getJSONArray("weather");
                 JSONObject weatherObject = weatherArray.getJSONObject(0);
                 String iconString = weatherObject.getString("icon");
+                String weatherDescription = weatherObject.getString("main");
 
-                Log.i("WORKED!!!----->", Double.toString(temp));
-                Log.i("WeatherWORKED!!!----->", weatherArray.toString());
-                Log.i("WeatherObjectWORKED!!>", weatherObject.toString());
-                Log.i("WeatherstringWORKED!!>", iconString);
+//                Log.i("WORKED!!!----->", Double.toString(temp));
+                Log.e("WORKED!!!--Name>", weatherDescription);
+//                Log.i("WeatherWORKED!!!----->", weatherArray.toString());
+//                Log.i("WeatherObjectWORKED!!>", weatherObject.toString());
+//                Log.i("WeatherstringWORKED!!>", iconString);
                 temperature.setText(Double.toString(temp));
+                temperature.setText(Double.toString(temp));
+                cityNameText.setText(cityName);
+                currentCity.setVisibility(View.VISIBLE);
+                mainIcon.setVisibility(View.VISIBLE);
+//                weatherTypeName.setVisibility(View.VISIBLE);
 
+//
 
+                String weatherType = iconString;
+//                weatherType = "Smoke"; THIS LINE IS USED TO TEST SWITCH CASE
+//                weatherTypeName.setText(weatherDescription);
+                Log.e("WORKED?--Name>", weatherType);
+                switch (weatherType) {
+                    case "01d":
+                        mainIcon.setImageResource(R.drawable.cloudy);
+                        break;
+                    case "13d":
+                        mainIcon.setImageResource(R.drawable.snow);
+                        break;
+                    case "02d":
+                        mainIcon.setImageResource(R.drawable.sunny);
+                        break;
+                    case "09n":
+                        mainIcon.setImageResource(R.drawable.rainy);
+                        break;
+                    case "11n":
+                        mainIcon.setImageResource(R.drawable.alert);
+                        break;
+                    case "50d":
+                        mainIcon.setImageResource(R.drawable.alert);
+                        break;
+                    default:
+                        mainIcon.setImageResource(R.drawable.clouded);
+                        break;
+                }
 
             } catch (JSONException e) {
                 String errorMessage = e.toString();
                 Log.e("ERROR----->", errorMessage);
+//
             }
         }
     }
